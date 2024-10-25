0fe5a09c717f2e50da583683d0d182006a28cf63
Carleoinserra <inserracarlo@gmail.com>
2024-10-25 10:02:38 +0200

    Add files via upload

---
+++ MyController.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+	
+	/*
+	 * Creiamo un oggetto di tipo dipJdbcTemplate
+	 */
+	dipJdbcTemplate d1;
+	/*
+	 * Questo oggetto lo iniettiamo nel controller tramite costruttore
+	 */
+	@Autowired
+	public MyController(dipJdbcTemplate d1) {
+		this.d1 = d1;
+	}
+	
+	
+	@GetMapping("/form")
+	public String getForm() {
+		
+		return "form";
+	}
+	/*
+	 * Il metodo submit riceve i dati dal form 
+	 */
+	@PostMapping("/submit")
+	public String getDip(@RequestParam("nome") String nome,
+			@RequestParam("mansione") String mansione,
+			@RequestParam("stipendio") String stipendio) {
+		// chiamiamo il metodo insertDip su d1 e li passiamo i dati ottenuti dal form
+		int stipendioD = Integer.parseInt(stipendio);
+		d1.insertDip(nome, mansione, stipendioD);
+		
+		
+		
+		
+		return "form";
+	}
+
+}
